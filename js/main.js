946522a485280d03636d030d4d63007df06b4d98
michaelshiko <michaelshiko2016@gmail.com>
2018-10-27 05:06:32 +0200

    Add ARIA roles - fix semantic rules

--- js/main.js
+++ js/main.js
@@ -168,7 +168,7 @@ createRestaurantHTML = (restaurant) => {
   const div = document.createElement('div');
   li.append(div);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   name.setAttribute('tabindex', '0');
   div.append(name);
